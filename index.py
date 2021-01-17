85266ef4a168e164e3e1b08c09abb661854dae29
Michaela <git@michaela.lgbt>
2021-01-17 17:28:58 +1000

    improvements

--- index.py
+++ index.py
@@ -3,17 +3,22 @@ from pygame.locals import *
 import random
 import time 
 import modem
+import pygame_gui
 
 BANDWIDTH = 2700
-BANDWIDTH_GUARD = 50 # the bit in the middle
+BANDWIDTH_GUARD = 200 # the bit in the middle
 BANDWIDTH_DEADZONE = 100 # the bits on the side
 ZOOM_DIVIDER = 2
 TONE_BANDWIDTH = (BANDWIDTH - BANDWIDTH_DEADZONE - BANDWIDTH_DEADZONE - BANDWIDTH_GUARD) /2
 DRAWABLE_SPACE = TONE_BANDWIDTH 
 TX_OFFSET = BANDWIDTH_DEADZONE
-
+SETTINGS_HEIGHT = 25
 swirly_things_draw = []
 swirly_things_rx = []
+
+# number of candidates to check 
+# CHECK_CANDIDATES = 5
+# candidates = []
 
 
 class swirly_thing(pygame.Rect):
@@ -23,47 +28,87 @@ class swirly_thing(pygame.Rect):
             self.center=location
             self.centerx+=random.randint(-2, 2)
             self.centery+=random.randint(-2, 2)
-            self.opacity = random.randint(100,255)
+            self.decay = 250
+            self.link_to_last = False
         def update(self):
-            self.opacity = self.opacity - random.randint(0,1)
+            self.decay = self.decay - random.randint(0,1)
 def xpos_to_tone(x):
     x = x * ZOOM_DIVIDER
     x = TX_OFFSET + x
     return x
 
 def ypos_to_tone(y):
-    y = y * ZOOM_DIVIDER
+    y = (y - SETTINGS_HEIGHT) * ZOOM_DIVIDER
     y = TX_OFFSET + TONE_BANDWIDTH + BANDWIDTH_GUARD +  y
     return y
+threshold = None
 
-def rx_callback(peaks_power_list, fft_data, freq):
+should_link = False
+
+def rx_callback(peaks_power_list, fft_data, freq, dbfs, power_nf):
+    global should_link, threshold
+    link = should_link
+    should_link = False
     try:
         tones = sorted([peaks_power_list[0], peaks_power_list[1]])
-        x= (tones[0]- TX_OFFSET)/ ZOOM_DIVIDER
-        y= (tones[1]  - TX_OFFSET - TONE_BANDWIDTH  - BANDWIDTH_GUARD ) / ZOOM_DIVIDER
-        location = (x+ (DRAWABLE_SPACE/ZOOM_DIVIDER),y )
-        print(location)
-        swirly_things_rx.append(swirly_thing(location))
-    except:
-        pass
+    except IndexError:
+        return
+    if freq[list(fft_data).index(max(fft_data))] > 3000: 
+        return
+    else:
+        
+        db_above_nf = int(max(fft_data) - power_nf)
+        print(db_above_nf)
+        try:
+            if db_above_nf < threshold.get_current_value(): # todo make this configurable
+                return
+        except AttributeError:
+            return
     
-    #print(peaks_power_list)
-    # check if we even have peaks in our desired range
-    # find drawable x / y from freq
-    # swirly_things_rx.append(swirly_thing((mouse_position)))
+        
+    # if freq[fft_data.index(max(fft_data))] > 3000:
+    #     return
+    x= (tones[0]- TX_OFFSET)/ ZOOM_DIVIDER
+    y= SETTINGS_HEIGHT + (tones[1]  - TX_OFFSET - TONE_BANDWIDTH  - BANDWIDTH_GUARD ) / ZOOM_DIVIDER
+
+    # check if in range
+    if tones[0] < TX_OFFSET or tones[0] > TX_OFFSET + TONE_BANDWIDTH:
+        return
+    if tones[1] < TX_OFFSET + TONE_BANDWIDTH + BANDWIDTH_GUARD or tones[1]  > TX_OFFSET + TONE_BANDWIDTH + BANDWIDTH_GUARD + TONE_BANDWIDTH:
+        return
     
+    location = (x+ (DRAWABLE_SPACE/ZOOM_DIVIDER),y)
+    swirly = swirly_thing(location)
+    swirly.decay = 5*db_above_nf
+    if link == True:
+        swirly.link_to_last = True
+    swirly_things_rx.append(swirly)
+    should_link = True
+
+    
+waterfall_surf = pygame.surface.Surface((int((DRAWABLE_SPACE/ZOOM_DIVIDER)*2), DRAWABLE_SPACE / ZOOM_DIVIDER))
+
+
+
 def main():
     # Initialise screen
+    global threshold
     pygame.init()
     clock = pygame.time.Clock()
-    screen = pygame.display.set_mode((int(DRAWABLE_SPACE), int(DRAWABLE_SPACE/ZOOM_DIVIDER)))
+    screen = pygame.display.set_mode((int((DRAWABLE_SPACE/ZOOM_DIVIDER)*2), int(DRAWABLE_SPACE/ZOOM_DIVIDER) + SETTINGS_HEIGHT))
     pygame.display.set_caption('Etch A TV')
 
     tones = modem.Modem(rx_callback)
     tones.sineFrequency2 = TX_OFFSET + TONE_BANDWIDTH + (BANDWIDTH_GUARD/2) # pilot tone
 
+    manager = pygame_gui.UIManager((int((DRAWABLE_SPACE/ZOOM_DIVIDER)*2), int(DRAWABLE_SPACE/ZOOM_DIVIDER) + SETTINGS_HEIGHT))
+    threshold = pygame_gui.elements.UIHorizontalSlider(relative_rect=pygame.Rect((0, 0), (int((DRAWABLE_SPACE/ZOOM_DIVIDER)*2), SETTINGS_HEIGHT)),
+                                             start_value=40, value_range=(0,100),
+                                             manager=manager)
+
     # Event loop
     while 1:
+        time_delta = clock.tick(60)/1000.0
         background = pygame.Surface(screen.get_size())
         background = background.convert()
         background.fill((255, 242, 255))
@@ -85,24 +130,32 @@ def main():
                 swirly_things_draw.append(swirly_thing((mouse_position)))
             if event.type == QUIT:
                 return
+            manager.process_events(event)
+        manager.update(time_delta)
         for x in swirly_things_draw:
             x.update()
-            if x.opacity <= 0:
+            if x.decay <= 0:
                 swirly_things_draw.remove(x)
             else:
-                pygame.draw.circle(background, (85, 205, 252), x.center, x.opacity/64)
+                pygame.draw.circle(background, (85, 205, 252), x.center, 1+(x.decay/64))
+        last_rx = None
         for x in swirly_things_rx:
             x.update()
-            if x.opacity <= 0:
+            if x.decay <= 0:
                 try:
-                    swirly_things_draw.remove(x)
+                    swirly_things_rx.remove(x)
                 except ValueError:
                     pass
             else:
-                pygame.draw.circle(background, (85, 205, 252), x.center, x.opacity/64)
+                pygame.draw.circle(background, (85, 205, 252), x.center, 1+(x.decay/64))
+            if last_rx and x.link_to_last:
+                pass
+                #pygame.draw.line(background, (85, 205, 252), x.center, last_rx.center,  1+int(x.decay/16))
+            last_rx = x
+        manager.draw_ui(background)
         screen.blit(background, (0, 0))
         pygame.display.flip()
-        clock.tick(50)
+        
 
 
 if __name__ == '__main__': main()
--- modem.py
+++ modem.py
@@ -13,7 +13,8 @@ class Modem():
         self.phase=0
         self.phase1=0
         self.phase2=0 # alignment
-        self.FRAMESPERBUFFER=4096
+        self.FRAMESPERBUFFER=1024
+        self.OVERLAP = 2 # over lap the FFT for RX.
         #self.RATE=44100
         self.RATE=48000
         self.WIDTH=2
@@ -21,37 +22,49 @@ class Modem():
         self.sineFrequency = freq1
         self.sineFrequency1 = freq2
         self.sineFrequency2 = freq3
+        self.last_data = []
+        for x in range(0,self.OVERLAP):
+            self.last_data.append(bytes(2*self.FRAMESPERBUFFER))
 
         self.callback = callback
-        self.window = signal.windows.hann(self.FRAMESPERBUFFER)
+        self.window = signal.windows.hann(self.FRAMESPERBUFFER * (self.OVERLAP + 1))
 
         self.stream = self.p.open(format=self.p.get_format_from_width(self.WIDTH),
                     channels=1, rate=self.RATE, output=True, input=True,
-                stream_callback=self.pacallback, frames_per_buffer=self.FRAMESPERBUFFER, input_device_index=5, output_device_index=6 )
+                stream_callback=self.pacallback, frames_per_buffer=self.FRAMESPERBUFFER, input_device_index=3, output_device_index=3 )
         
         self.running = False
 
+        self.fftfreq = np.fft.fftshift(np.fft.fftfreq(self.FRAMESPERBUFFER*(self.OVERLAP + 1))) * self.RATE
+        self.fftfreq = self.fftfreq[int(len(self.fftfreq)/2):]
+
     def pacallback(self, in_data, frame_count, time_info, status):
-        data = numpy.frombuffer(in_data, dtype=numpy.int16) * self.window
-        fft_data = 20 * np.log10(abs(np.fft.fft(data).real)) - 20 * np.log10(self.FRAMESPERBUFFER)
-        fft_data = fft_data[:int(len(fft_data)/2)] 
-        freq = np.fft.fftfreq(self.FRAMESPERBUFFER,1.0/self.RATE)
-        freq = freq[:int(len(freq)/2)]
-        peak_index, dict_vals = signal.find_peaks(fft_data, prominence=20, distance=50)
-        peaks = {freq[x]: fft_data[x] for x in peak_index}
+        raw_buffer = b''.join(self.last_data) + in_data
+        data = numpy.frombuffer(raw_buffer, dtype=numpy.int16) * self.window
+        data = data.astype(np.float64) / (2 ** 15)
+        self.last_data.pop(0)
+        self.last_data.append(in_data)
+        fft_data = 20 * np.log10(
+                abs(np.fft.fftshift(np.fft.fft(data)))
+        ) - 20 * np.log10(self.FRAMESPERBUFFER*(self.OVERLAP+ 1))
+        fft_data = fft_data[int(len(fft_data)/2):]
+        dbfs = 20*np.log10(data.max())
+        power_nf = np.mean(fft_data)
+        peak_index, dict_vals = signal.find_peaks(fft_data, prominence=10, distance=5)
+        #print(peak_index)
+        peaks = {self.fftfreq [x]: fft_data[x] for x in peak_index}
         peaks_power_list = sorted(peaks, key=peaks.get, reverse=True)
-        self.callback(peaks_power_list, fft_data, freq)
+        self.callback(peaks_power_list, fft_data, self.fftfreq, dbfs, power_nf )
 
         #  generate output
         outbuf = bytes()
-        ampltitude = 32767/3 # because we have three tones
+        ampltitude = 65535/2 # because we have three tones
         #frame_count
         if self.running:
             for n in range(frame_count):
                 outbuf += int(
-                    ampltitude * 0.3 * numpy.sin(self.phase) + 
-                    ampltitude * 0.3 * numpy.sin(self.phase1) +
-                    ampltitude * 0.3 * numpy.sin(self.phase2) 
+                    ampltitude * 0.5 * numpy.sin(self.phase) + 
+                    ampltitude * 0.5 * numpy.sin(self.phase1) 
                 ).to_bytes(2, byteorder='little', signed=True)
                 self.phase += 2*numpy.pi*self.sineFrequency/self.RATE
                 self.phase1 += 2*numpy.pi*self.sineFrequency1/self.RATE
